fff15505b70a88c6e0f14a8e25694c6a0f9244ee
9527Andrew <105211431+9527Andrew@users.noreply.github.com>
2023-11-08 17:07:45 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -14,19 +14,30 @@ plot(out.ScopeData.time,out.ScopeData.signals(1).values(:,3),'LineWidth',1.5)
 hold on
 legend('H2','O2','H2O')
 title('Mass Fractions')
+set(gca,'FontName','Arial','FontSize',12)
+xlabel('Time(s)')
+ylabel('MassFraction')
+exportgraphics(gcf,'mass fraction.pdf','ContentType','vector');
 %% temp
 figure(2)
 plot(out.ScopeData.time,out.ScopeData.signals(2).values,'LineWidth',1.5)
 hold on
 legend('T')
 title('Temperatures')
+set(gca,'FontName','Arial','FontSize',12)
+xlabel('Time(s)')
+ylabel('Tempreture')
+exportgraphics(gcf,'tempreture.pdf','ContentType','vector');
 %%
 figure(3)
 plot(out.ScopeData.time,out.ScopeData.signals(3).values,'LineWidth',1.5)
 hold on
 legend('P')
 title('Pressure')
-
+set(gca,'FontName','Arial','FontSize',12)
+xlabel('Time(s)')
+ylabel('Pressure')
+exportgraphics(gcf,'pressure.pdf','ContentType','vector');
 
 %% 画图
 % clf
